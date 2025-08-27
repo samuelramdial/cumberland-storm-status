05621af75273c1da1919faf41e2343a79c2e98bd
samuelramdial <samuelramdial0327@gmail.com>
2025-08-26 21:52:41 -0400

    client: fix ESLint deps; remove overrides; add @types/leaflet

--- src/client/eslint.config.js
+++ src/client/eslint.config.js
@@ -1,4 +1,3 @@
-// ESLint 9 flat config for Vite + React + TypeScript
 import js from '@eslint/js'
 import globals from 'globals'
 import tsParser from '@typescript-eslint/parser'
@@ -8,45 +7,30 @@ import reactHooks from 'eslint-plugin-react-hooks'
 
 export default [
   { ignores: ['dist/**', 'node_modules/**'] },
-
   {
     files: ['src/**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 'latest',
       sourceType: 'module',
       parser: tsParser,
-      globals: {
-        ...globals.browser,
-        ...globals.es2021
-      }
+      globals: { ...globals.browser, ...globals.es2021 }
     },
     plugins: {
       '@typescript-eslint': tsPlugin,
       react: reactPlugin,
       'react-hooks': reactHooks
     },
-    settings: {
-      react: { version: 'detect' }
-    },
+    settings: { react: { version: 'detect' } },
     rules: {
-      // Base JS recommendations
       ...js.configs.recommended.rules,
-
-      // Disable base rules that clash with TS-aware versions
       'no-undef': 'off',
       'no-unused-vars': 'off',
-
-      // TS-aware unused-vars (enforced)
       '@typescript-eslint/no-unused-vars': [
         'error',
         { argsIgnorePattern: '^_', varsIgnorePattern: '^_', caughtErrors: 'none' }
       ],
-
-      // React
       'react/react-in-jsx-scope': 'off',
       'react/prop-types': 'off',
-
-      // React Hooks
       'react-hooks/rules-of-hooks': 'error',
       'react-hooks/exhaustive-deps': 'warn'
     }
